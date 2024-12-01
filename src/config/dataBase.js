17b881f14f47b764f289951b33fbf02f982696c0
MUHAMMAD TALHA ARSHAD <talhaarshad010@gmail.com>
2024-12-01 12:49:21 +0500

    12:49

--- src/config/dataBase.js
+++ src/config/dataBase.js
@@ -1,13 +1,33 @@
+// const mongoose = require('mongoose');
+// require('dotenv').config();
+// console.log('first', process.env.DB);
+// const connectDB = async () => {
+//   await mongoose.connect(process.env.DB);
+// };
+
+// connectDB().then(data => {
+//   console.log('Database is Connected');
+// });
+
+// connectDB().catch(err => {
+//   console.log(err.message, 'Databse is not connected');
+// });
+
 const mongoose = require('mongoose');
+require('dotenv').config(); // Ensure .env is loaded
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.DB);
+  try {
+    if (!process.env.DB) {
+      throw new Error(
+        'Database URI is not defined in the environment variables',
+      );
+    }
+    await mongoose.connect(process.env.DB);
+    console.log('Database is connected');
+  } catch (err) {
+    console.log(err.message, 'Database is not connected');
+  }
 };
 
-connectDB().then(data => {
-  console.log('Database is Connected');
-});
-
-connectDB().catch(err => {
-  console.log(err.message, 'Databse is not connected');
-});
+connectDB();
